7884a33cbd27f86fc9417eed17a13ee2938a90df
Anna <cameaann@gmail.com>
2024-03-06 10:48:23 +0200

    added Note component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+/* eslint-disable react/prop-types */
 
-const App = () => {
+import Note from "./components/Note";
 
+const App = (props) => {
+  const { notes } = props;
+
+  const result = notes.map(note => note.content)
+  console.log(result)
 
   return (
     <div>
-     
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note}/>
+        ))}
+      </ul>
+      
     </div>
-    
-  )
+  );
 };
 
-
-export default App
+export default App;
