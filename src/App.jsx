285fec0d9f46c3d214b1b93fb4e3480c481ce2e4
Anna <cameaann@gmail.com>
2024-03-15 10:23:51 +0200

    added onFocus into the input

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,32 @@ import Note from "./components/Note";
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
-  const [showAll, setShowAll] = useState(true)
+  const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  console.log('render', notes.length, 'notes')
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+  console.log("render", notes.length, "notes");
 
   const notesToShow = showAll
     ? notes
-    : notes.filter(note => note.important === true)
+    : notes.filter((note) => note.important === true);
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
       id: notes.length + 1,
-    }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
@@ -45,7 +43,7 @@ const App = () => {
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? "important" : "all"}
         </button>
       </div>
       <ul>
@@ -54,7 +52,12 @@ const App = () => {
         ))}
       </ul>
       <form onSubmit={addNote}>
-        <input type="text" value={newNote} onChange={handleNoteChange} />
+        <input
+          type="text"
+          value={newNote}
+          onChange={handleNoteChange}
+          onFocus={(e) => (e.target.value = "")}
+        />
         <button type="submit">Save</button>
       </form>
     </div>
